f2c8b8814a564e3adc1012f08003dd32c02b8c99
Harvin Bejarano <harvinbejarano@hotmail.com>
2018-12-09 11:31:59 -0500

    adding dashboard component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { NgModule } from '@angular/core';
 
 @NgModule({
-  declarations: [],
+  declarations: [DashboardComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,MatCardModule,MatButtonModule,MatIconModule
+  ],
+  exports: [DashboardComponent],
 })
 export class HomeModule { }
